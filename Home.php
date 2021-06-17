08670c47ce92a4d1cf75c9781b7496cbe23e7a1b
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2021-06-17 10:28:52 +0530

    Update Home.php

--- Home.php
+++ Home.php
@@ -103,13 +103,13 @@ else
  {
   echo "<script> var i='{$local_url}/checklogin.php';window.location.href=i;
   function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
+    var cookies = document.cookie.split(';');
 
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
+        var eqPos = cookie.indexOf('=');
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
     }
 }
 
@@ -120,13 +120,13 @@ else
 {
    echo "<script> var i='{$local_url}/checklogin.php';window.location.href=i; 
    function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
+    var cookies = document.cookie.split(';');
 
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
+        var eqPos = cookie.indexOf('=');
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
     }
 }
 
